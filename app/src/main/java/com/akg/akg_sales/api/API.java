4665c0052dad15b64ffb0428c13b6e7e2286fe75
DELL <fahimbuet1995@gmail.com>
2024-06-01 14:20:17 +0600

    - Test Release for Complaint Handling application integration testing   Version Code: 21   Version Name: 2.5.11-test

--- app/src/main/java/com/akg/akg_sales/api/API.java
+++ app/src/main/java/com/akg/akg_sales/api/API.java
@@ -38,8 +38,8 @@ public class API {
 
 //    public static String baseUrl = devEnvUrl;
 
-    public static String baseUrl = testServerLocalAccess;
-//    public static String baseUrl = testServerPublicAccess;
+//    public static String baseUrl = testServerLocalAccess;
+    public static String baseUrl = testServerPublicAccess;
 
 //    public static String baseUrl = cgdProdServerPublicAccess;
 
